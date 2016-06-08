cc5f02c6a1037f9e1bc84591d3709af7bf94180f
zjx <zhao_j1@denison.edu>
2016-06-08 15:41:37 -0500

    done

--- resource.py
+++ resource.py
@@ -4,17 +4,20 @@ def car(env, name, bcs, driving_time, charge_duration):
     yield env.timeout(driving_time)
 
     print('%s arriving at %d' % (name, env.now))
-    with bcs.request() as req:
-        print bcs.put_queue, "This is the put queue"
-        print bcs.users, "These are the users"
-        print bcs.capacity, "This is the capacity"
-        print bcs.get_queue
-        yield req
-        print req, "This is request"
+    #with bcs.request() as req:
+    req = bcs.request()
+    # print bcs.put_queue, "This is the put queue"
+    print bcs.users, "These are the users"
+    # print bcs.capacity, "This is the capacity"
+    # print bcs.get_queue
+    #Need to yield the request so it gets the resource
+    yield req
+    print req, "This is request"
 
-        print('%s starting to charge at %s' % (name, env.now))
-        yield env.timeout(charge_duration)
-        print('%s leaving the bcs at %s' % (name, env.now))
+    print('%s starting to charge at %s' % (name, env.now))
+    yield env.timeout(charge_duration)
+    print('%s leaving the bcs at %s' % (name, env.now))
+    bcs.release(req)
 
 
 if __name__ == "__main__":
